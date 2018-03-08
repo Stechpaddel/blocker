bf37b6fd43abf4573b36f45e80c9ab2cef138451
root <root@testcontainer.intern>
2018-03-08 13:41:49 +0000

    update

--- blocker-install.sh
+++ blocker-install.sh
@@ -4,13 +4,27 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #installscript
 
 #create directory
+blocker_dir="/etc/blocker"
+if [ ! -d  "$blocker_dir" ]
+then
 mkdir /etc/blocker
+fi
 
 #create subdirectory for the files to geoblock
-mkdir /etc/blocker/contries
+blocker_sub_dir="/etc/blocker/countries"
+if [ ! -d  "$blocker_sub_dir" ]
+then
+mkdir /etc/blocker/countries
+fi
+
 
 #get the zonefiles 
+all_zones="/etc/blocker/countries/all-zones.tar.gz"
+if [ ! -f  "$all_zones" ]
+then
 wget -P /etc/blocker/contries/ http://www.ipdeny.com/ipblocks/data/countries/all-zones.tar.gz
+fi
+
 
 #create subdirectory for the unpacked zone files
 mkdir /etc/blocker/contries/zones
--- geoblock_helper2.sh
+++ geoblock_helper2.sh
@@ -3,5 +3,14 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 for var in "$@"
 do
-echo $var >> /etc/blocker/unblock_countries_ips.txt 
+#echo $var >> /etc/blocker/unblock_countries_ips.txt 
+iptables -I blocker -s $var -p TCP --dport 80 -j ACCEPT
 done
+
+exist="$(iptables -L blocker -n | grep RETURN | awk '{print $1}' | sed '2,$d')" 
+comp="RETURN"
+
+if [ "$exist" != "$comp" ] 
+then
+iptables -A blocker -j RETURN
+fi
