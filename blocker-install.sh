a3432b5d420d40af8879c9837268a5a13a4a51bd
root <root@testcontainer.intern>
2018-03-09 10:18:42 +0000

    updated

--- blocker-install.sh
+++ blocker-install.sh
@@ -35,7 +35,7 @@ fi
 #mkdir /etc/blocker/contries/zones
 
 #unpack the zonefiles
-tar -xzf /etc/blocker/contries/all-zones.tar.gz -C /etc/blocker/contries/zones/
+tar -xzf /etc/blocker/countries/all-zones.tar.gz -C /etc/blocker/countries/zones/
 
 #create the geoblock configfile
 curl http://www.ipdeny.com/ipblocks/ 2>/dev/null |grep "<tr><td><p>" | sed 's/<tr><td><p>//' | sed 's/<table cellspacing=1 cellpadding1 border=1>//' | sed 's/<.*//' | sed 's/.$//' | cat >> /etc/blocker/geoblock.conf
