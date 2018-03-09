56f10fe7c76f28b58ea54b146ebbb2d5d5249a08
root <root@testcontainer.intern>
2018-03-09 10:22:10 +0000

    updated

--- geoblock_helper.sh
+++ geoblock_helper.sh
@@ -5,6 +5,6 @@ for var in "$@"
 do
    # echo "$var"
 unblock_ips="$(cat /etc/blocker/contries/zones/$var.zone)"
-sh /root/blocker/geoblock_helper2.sh $unblock_ips
+/etc/blocker/geoblock_helper2.sh $unblock_ips
 #echo $unblock_ips
 done
