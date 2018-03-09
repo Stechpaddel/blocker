931ed59f976b921cbe2a16e8112d8d1c54fad577
root <root@testcontainer.intern>
2018-03-09 08:52:38 +0000

    updated

--- blocker-worker.sh
+++ blocker-worker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-s5="$(iptables -L -n | grep blocker-white | awk '{print $2}')"
+s5="$(iptables -L -n | grep blocker-white | grep Chain | awk '{print $2}')"
 s6="blocker-white"
 
-if [ "$s3" != "$s4" ];
+if [ "$s5" != "$s6" ];
 then
 iptables -N blocker-white
 iptables -t filter -A INPUT -j blocker-white
 fi
 
-s3="$(iptables -L -n | grep blocker-scan | awk '{print $2}')"
+s3="$(iptables -L -n | grep blocker-scan | grep Chain | awk '{print $2}')"
 s4="blocker-scan"
 
 if [ "$s3" != "$s4" ];
@@ -20,14 +20,14 @@ iptables -t filter -A INPUT -j blocker-scan
 fi
 
 #grep fatal messages frpm authlog
-bad_ips="$(grep "fatal" /var/log/auth.log | awk '{print $11 }')"
+bad_ips="$(grep "fatal" /var/log/auth.log | awk '{print $11 }' | uniq)"
 #echo $bad_ips
 
 /root/blocker/portscan_helper.sh $bad_ips
 
 
 #create the chaon fpr the blocked ips
-s1="$(iptables -L -n | grep blocker-geo | awk '{print $2}')"
+s1="$(iptables -L -n | grep blocker-geo | grep Chain | awk '{print $2}')"
 s2="blocker-geo"
 
 if [ "$s1" != "$s2" ];
