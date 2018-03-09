8a876ca7f865e42d280424701469cb44c54ddd4c
Stechpaddel <root@testcontainer.intern>
2018-03-09 15:10:56 +0000

    add duplicate check

--- whitelist_helper.sh
+++ whitelist_helper.sh
@@ -3,7 +3,14 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 for var in "$@"
 do
+dub_check="$(echo $var)"
+dub_comp="$(iptables -L blocker-white -n | grep $var | uniq | awk ' {print $4}')"
+
+if [ "$dub_check" != "$dub_comp" ];
+then
 iptables -I blocker-white -s $var -p TCP -j ACCEPT
+fi
+
 done
 
 exist="$(iptables -L blocker-white -n | grep RETURN | awk '{print $1}' | sed '2,$d')" 
