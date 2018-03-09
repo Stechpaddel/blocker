e610c0a99f240528696538c9b95471faf12a2e9c
root <root@testcontainer.intern>
2018-03-09 10:29:46 +0000

    updated

--- geoblock_helper.sh
+++ geoblock_helper.sh
@@ -4,7 +4,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 for var in "$@"
 do
    # echo "$var"
-unblock_ips="$(cat /etc/blocker/contries/zones/$var.zone)"
+unblock_ips="$(cat /etc/blocker/countries/zones/$var.zone)"
 /etc/blocker/geoblock_helper2.sh $unblock_ips
 #echo $unblock_ips
 done
