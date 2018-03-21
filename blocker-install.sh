c52e051c9884015f3a19dc59c445d36e89f435f0
Stechpaddel <root@testcontainer.intern>
2018-03-21 11:32:41 +0000

    delete worker and expand installer

--- blocker-install.sh
+++ blocker-install.sh
@@ -51,3 +51,44 @@ cp ./blocker/geoblock-worker.sh /etc/blocker/geoblock-worker.sh
 cp ./blocker/scanblock-worker.sh /etc/blocker/scanblock-worker.sh
 cp ./blocker/whitelist-worker.sh /etc/blocker/whitelist-worker.sh
 
+#create iptables chains
+
+#allow established connections
+s9="$(iptables -L -n | grep ESTABLISHED | awk '{print $2}')"
+s10="RELATED,ESTABLISHED"
+
+if [ "$s5" != "$s6" ];
+then
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+fi
+
+#create whitelist chain
+
+s5="$(iptables -L -n | grep blocker-white | grep Chain | awk '{print $2}')"
+s6="blocker-white"
+
+if [ "$s5" != "$s6" ];
+then
+iptables -N blocker-white
+iptables -t filter -A INPUT -j blocker-white
+fi
+
+#create portscan block chain 
+s3="$(iptables -L -n | grep blocker-scan | grep Chain | awk '{print $2}')"
+s4="blocker-scan"
+
+if [ "$s3" != "$s4" ];
+then
+iptables -N blocker-scan
+iptables -t filter -A INPUT -j blocker-scan
+fi
+
+#create geoblock chain
+s1="$(iptables -L -n | grep blocker-geo | grep Chain | awk '{print $2}')"
+s2="blocker-geo"
+
+if [ "$s1" != "$s2" ];
+then
+iptables -N blocker-geo
+iptables -t filter -A INPUT -j blocker-geo
+fi
