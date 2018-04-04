08e5113654635b636b8d46f2a2e695fbd2cc4f32
Stechpaddel <root@testcontainer.intern>
2018-04-04 19:15:22 +0000

    add logging

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ blocker_dir="/etc/blocker"
 if [ ! -d  "$blocker_dir" ]
 then
 mkdir /etc/blocker
+echo blocker directory created >> /etc/blocker/blocker-log.txt
 fi
 
 #create subdirectory for the files to geoblock
@@ -15,6 +16,7 @@ blocker_sub_dir="/etc/blocker/countries"
 if [ ! -d  "$blocker_sub_dir" ]
 then
 mkdir /etc/blocker/countries
+echo blocker subdirectory countries created >> /etc/blocker/blocker-log.txt
 fi
 
 
@@ -23,6 +25,7 @@ fi
 if [ ! -f  /etc/blocker/countries/all-zones.tar.gz ]
 then
 wget -P /etc/blocker/countries/ http://www.ipdeny.com/ipblocks/data/countries/all-zones.tar.gz
+echo all zone file downloaded >> /etc/blocker/blocker-log.txt
 fi
 
 
@@ -31,26 +34,35 @@ blocker_sub_dir2="/etc/blocker/countries/zones"
 if [ ! -d  "$blocker_sub_dir2" ]
 then
 mkdir /etc/blocker/countries/zones
+echo blocker saubdirectory country zones created >> /etc/blocker/blocker-log.txt
 fi
 #mkdir /etc/blocker/contries/zones
 
 #unpack the zonefiles
 tar -xzf /etc/blocker/countries/all-zones.tar.gz -C /etc/blocker/countries/zones/
+echo unpack all zones file >> /etc/blocker/blocker-log.txt
 
 #create the geoblock configfile
 curl http://www.ipdeny.com/ipblocks/ 2>/dev/null |grep "<tr><td><p>" | sed 's/<tr><td><p>//' | sed 's/<table cellspacing=1 cellpadding1 border=1>//' | sed 's/<.*//' | sed 's/.$//' | cat >> /etc/blocker/geoblock.conf
+echo create countrylist  >> /etc/blocker/blocker-log.txt
 
 #create config for port opening in geoip whitelistning
 touch /etc/blocker/geoblock_open_ports.conf
+echo create geoblock_open_ports.conf  >> /etc/blocker/blocker-log.txt
 
 #create whitelis ip list
 touch /etc/blocker/whitelist.txt
+echo create whitelist.txt >> /etc/blocker/blocker-log.txt
 
 #copy the other scrips in the directory
 cp ./blocker/all_zones-update.sh /etc/blocker/all_zones-update.sh
+echo copy all_zones-update.sh >> /etc/blocker/blocker-log.txt
 cp ./blocker/geoblock-worker.sh /etc/blocker/geoblock-worker.sh
+echo copy geoblock-worker.sh  >> /etc/blocker/blocker-log.txt
 cp ./blocker/scanblock-worker.sh /etc/blocker/scanblock-worker.sh
+echo copy scanblock-worker.sh  >> /etc/blocker/blocker-log.txt
 cp ./blocker/whitelist-worker.sh /etc/blocker/whitelist-worker.sh
+echo copy whitelist-worker.sh >> /etc/blocker/blocker-log.txt
 
 #create iptables chains
 
@@ -61,6 +73,7 @@ s10="RELATED,ESTABLISHED"
 if [ "$s9" != "$s10" ];
 then
 iptables -I INPUT 1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+echo add established connection accept >> /etc/blocker/blocker-log.txt
 fi
 
 #create whitelist chain
@@ -71,6 +84,7 @@ s6="blocker-white"
 if [ "$s5" != "$s6" ];
 then
 iptables -N blocker-white
+echo create blocker-white chain >> /etc/blocker/blocker-log.txt
 fi
 
 
@@ -81,6 +95,7 @@ s4="blocker-scan"
 if [ "$s3" != "$s4" ];
 then
 iptables -N blocker-scan
+echo create blocker-scan chain >> /etc/blocker/blocker-log.txt
 fi
 
 #create geoblock chain
@@ -90,6 +105,7 @@ s2="blocker-geo"
 if [ "$s1" != "$s2" ];
 then
 iptables -N blocker-geo
+echo create blocker-geo chain >> /etc/blocker/blocker-log.txt
 fi
 
 
@@ -105,5 +121,9 @@ iptables -t filter -I INPUT 4 -j blocker-geo
 iptables -A blocker-white -j RETURN
 iptables -A blocker-scan -j RETURN
 iptables -A blocker-geo -j RETURN
+echo create filter and return ruled >> /etc/blocker/blocker-log.txt
 fi
 
+
+#create logfile
+echo blocker succsessfully installed >> /etc/blocker/blocker-log.txt
